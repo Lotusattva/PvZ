d412661bfd96e086095e3e63390b4834dfa6aaaf
Jingwei Chen <cjw990802@gmail.com>
2024-11-13 23:33:42 -0800

    Refactor ShooterPlant class: add projectile type parameter to constructor and include Projectile header

--- inc/ShooterPlant.hpp
+++ inc/ShooterPlant.hpp
@@ -2,14 +2,16 @@
 #define SHOOTER_PLANT_HPP
 
 #include "Plant.hpp"
+#include "Projectile.hpp"
 
 class ShooterPlant : public Plant {
 private:
+    const ProjectileType projectileType;
     const short int range;
     const short int cooldown;
 public:
-    ShooterPlant(short int health, short int col, short int row, short int range, short int cooldown) :
-        Plant(health, col, row), range(range), cooldown(cooldown) {}
+    ShooterPlant(short int health, short int col, short int row, ProjectileType projectileType, short int range, short int cooldown) :
+        Plant(health, col, row), projectileType(projectileType), range(range), cooldown(cooldown) {}
     virtual ~ShooterPlant() = 0 {}
 
     virtual void shoot();

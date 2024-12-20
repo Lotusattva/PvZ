57a5b376ea0167c1952387eb13430c4ba0cb8a17
Jingwei Chen <cjw990802@gmail.com>
2024-12-20 13:15:57 -0800

    fix path

--- src/zombies/RegularZombie.cpp
+++ src/zombies/RegularZombie.cpp
@@ -3,18 +3,19 @@
 namespace PvZ {
 
     RegularZombie::Textures::Textures() {
-        string path{ "res/img/zombie_idle/" }, ext{ ".png" };
+        string root_path{ "res/img/zombie_" }, idle_path{ root_path + "idle/" }, walk_path{ root_path + "walk/" },
+            attack_path{ root_path + "attack/" }, death_path{ root_path + "death/" }, ext{ ".png" };
         for (short i : range(1, IDLE_FRAMES + 1)) {
-            idle[i].loadFromFile(path + to_string(i) + ext);
+            idle[i].loadFromFile(idle_path + to_string(i) + ext);
         }
         for (short i : range(1, WALK_FRAMES + 1)) {
-            walk[i].loadFromFile(path + to_string(i) + ext);
+            walk[i].loadFromFile(walk_path + to_string(i) + ext);
         }
         for (short i : range(1, ATTACK_FRAMES + 1)) {
-            attack[i].loadFromFile(path + to_string(i) + ext);
+            attack[i].loadFromFile(attack_path + to_string(i) + ext);
         }
         for (short i : range(1, DEATH_FRAMES + 1)) {
-            death[i].loadFromFile(path + to_string(i) + ext);
+            death[i].loadFromFile(death_path + to_string(i) + ext);
         }
     }
 

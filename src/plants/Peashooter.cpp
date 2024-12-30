1e0fb771e901fc59b5e5f44dd6c1522fd0bc952e
Jingwei Chen <cjw990802@gmail.com>
2024-12-29 18:32:06 -0800

    remove definition of defaulted destructor

--- src/plants/Peashooter.cpp
+++ src/plants/Peashooter.cpp
@@ -14,8 +14,6 @@ namespace PvZ {
     Peashooter::Peashooter(short row) :
         ShooterPlant{ 6, row, Projectile::Type::PEA, 8, 1000ms } {}
 
-    Peashooter::~Peashooter() {}
-
     bool Peashooter::action() {
         if (!isAlive()) {
             return false;
